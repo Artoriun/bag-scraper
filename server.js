792ae50a203a9205986e55413fbddae51810704d
Peter de Keijzer <pjcr.dekeijzer@gmail.com>
2024-11-08 16:33:44 +0900

    Added Netlify configuration

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.use(cors({
   origin: ['http://localhost:3000', 'https://*.herokuapp.com'],
   credentials: true
